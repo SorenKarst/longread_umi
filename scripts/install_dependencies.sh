199db6492aca0ff5a553340547ab9a51ddc83451
smk <smk@bio.aau.dk@a512-gc1-d-01.srv.aau.dk>
2019-08-14 13:09:16 +0200

    Fixed error in dependency transfer to dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -153,7 +153,7 @@ ln -s $SOFTWARE_DIR/htslib_1.9/bin/bgzip ~/bin/bgzip
 
 # Medaka
 conda create -c bioconda -n medaka medaka
-echo "export MEDAKA_ENV_START='eval \"$(conda shell.bash hook)\"; conda activate medaka'" >> ./longread-UMI-pipeline_paths.txt
+echo "export MEDAKA_ENV_START='eval \"\$(conda shell.bash hook)\"; conda activate medaka'" >> ./longread-UMI-pipeline_paths.txt
 echo "export MEDAKA_ENV_STOP='conda deactivate'" >> ./longread-UMI-pipeline_paths.txt
 echo "export MEDAKA_MODEL=r941_min_high" >> ./longread-UMI-pipeline_paths.txt
 
@@ -207,6 +207,5 @@ sed -i \
 
 ### Test longread-UMI-pipeline
 cd longread-UMI-pipeline/test_data
-longread_umi nanopore_pipeline -d test_reads.fq -s 10 -c 30 -t 1
+longread_umi nanopore_pipeline -d test_reads.fq -o . -c 30 -w rrna_operon -t 1
 longread_umi qc_pipeline -d test_reads.fq -c consensus_racon_medaka_medaka.fa -r zymo_curated -t 1
-
