a9c4a6aa5a5abd8f3e0686442f1a58133789487b
ziels <ziels@mail.ubc.ca>
2019-12-06 11:48:40 -0800

    Update consensus_racon.sh

--- scripts/consensus_racon.sh
+++ scripts/consensus_racon.sh
@@ -8,7 +8,7 @@
 #
 # IMPLEMENTATION
 #    author	Søren Karst (sorenkarst@gmail.com)
-#               Ryans Ziels (ziels@mail.ubc.ca)
+#               Ryan Ziels (ziels@mail.ubc.ca)
 #    license	GNU General Public License
 
 ### Terminal input ------------------------------------------------------------
